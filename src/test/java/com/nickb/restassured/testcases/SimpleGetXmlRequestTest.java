69c3ec65fea1583ea59410c79f63f55c2e3a1c1b
Nickolay Biadrytski <nickbiadrytski@gmail.com>
2019-01-24 02:17:03 +0300

    added jsonobject handling to test

--- src/test/java/com/nickb/restassured/testcases/SimpleGetXmlRequestTest.java
+++ src/test/java/com/nickb/restassured/testcases/SimpleGetXmlRequestTest.java
@@ -3,49 +3,37 @@ package com.nickb.restassured.testcases;
 import com.nickb.restassured.base.BaseTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
-import io.restassured.path.xml.XmlPath;
-import io.restassured.path.xml.config.XmlPathConfig;
 import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.json.XML;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
 
-public class SimpleGetXmlRequestTest extends BaseTest{
+public class SimpleGetXmlRequestTest extends BaseTest {
 
     @Test
-    public void checkMicaXmlResponseTest() throws IOException {
+    public void checkMicaXmlResponseTest() {
 
         RestAssured.baseURI = envConfig.getProperty("s_prod-https");
         Response resp = given().relaxedHTTPSValidation().
                 param("SMADnoads", true).
-            when().
+                when().
                 get(envConfig.getProperty("s_endpoint-path") + "vmap.xml").
-            then().
+                then().
                 assertThat().statusCode(200).and().
                 contentType(ContentType.XML).and().
                 extract().response();
 
         String responseString = resp.asString();
 
-        //XmlPath xmlPath = new XmlPath(responseString).using(XmlPathConfig.xmlPathConfig()).
-        //XmlPath xmlPath = new XmlPath(responseString).using(XmlPathConfig.xmlPathConfig().declaredNamespace("smls", "http://seamless.net/smls"));
-        //XmlPath xmlPath = new XmlPath(responseString).using(XmlPathConfig.xmlPathConfig().namespaceAware(false));
-        //xmlPath.setRoot()
-        //System.out.println(xmlPath.getString("vmap:VMAP.vmap:Extensions.smls:content"));
-        //System.out.println(xmlPath.getString("vmap:VMAP.vmap:Extensions.smls:content.@contentUri"));
-
-        //XmlPath micaXml = new XmlPath(responseString);
-        //System.out.println("-----------" + micaXml.get(".//content[@contentUri]"));
-        //System.out.println("-------------------" + uuid);
-
-        // gut seamless uuid
-        //JsonPath micaJson = new JsonPath(responseString);
-        //String documentid = micaJson.get("documentid");
+        // convert xml to json
+        JSONObject jsonResult = XML.toJSONObject(responseString);
+        JSONObject vmap = jsonResult.getJSONObject("vmap:VMAP").getJSONObject("vmap:Extensions").getJSONObject("smls:extensions").getJSONObject("smls:content");
+        String uuid = vmap.getString("contextId");
+        System.out.println(uuid);
 
         // call master.m3u8 with extracted uuid
         /*given().relaxedHTTPSValidation().param("proxy", false).
